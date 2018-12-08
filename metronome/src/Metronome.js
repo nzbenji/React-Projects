8320df61e043237174704d2d79e38cc8e40e6b08
Ben <benlittle53@gmail.com>
2018-12-09 11:34:21 +1300

    timer is now playing beats depending on bpm

--- metronome/src/Metronome.js
+++ metronome/src/Metronome.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Metronome.css';
+import click1 from './click1.wav';
+import click2 from './click2.wav';
 
 class Metronome extends React.Component {
         state = {
@@ -9,6 +11,41 @@ class Metronome extends React.Component {
             beatsPerMeas: 4
     }
 
+    clickOne = new Audio(click1)
+    clickTwo = new Audio(click2)
+
+    togglePlayStop = _ => {
+        //Stop timer button and render isPlaying state to false
+        if(this.state.isPlaying){
+            clearInterval(this.timer);
+            this.setState({
+                isPlaying: false
+            })
+        } else {
+            //start timer button and set a timer depending on bpm value
+            this.timer = setInterval(
+                this.playClick, (60 / this.state.bpm ) * 1000
+            );
+            this.setState({
+                count: 0, //tracks which beat we're on
+                isPlaying: true
+            })
+        }
+    }
+
+    playClick = _ => {
+        this.state.count & this.state.beatsPerMeas === 0 ? this.clickTwo.play() : this.clickOne.play();
+
+        this.setState({
+            count: (this.state.count + 1) % this.state.beatsPerMeas
+        })
+    }
+    
+    handleSlider = event => {
+        const bpm = event.target.value
+        this.setState({ bpm })
+    }
+
     render() {
         const { isPlaying, bpm } = this.state
 
@@ -16,9 +53,17 @@ class Metronome extends React.Component {
             <div className="container">
                 <div className="bpm__slider">
                     <div>{bpm} BPM</div>
-                    <input type="range" min="60" max="240" value={bpm} />
+                    <input 
+                    type="range" 
+                    min="60" 
+                    max="240" 
+                    value={bpm}
+                    onChange={ this.handleSlider }
+                    />
                 </div>
-                <button> { isPlaying ? 'Stop' : 'Start' } </button>
+                <button onClick={ this.togglePlayStop }> 
+                { isPlaying ? 'Stop' : 'Start' } 
+                </button>
             </div>
         )
     }
