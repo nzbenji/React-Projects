a0c25f779f577e4a020361e0f7feb399ddde85ac
Ben <benlittle53@gmail.com>
2018-12-16 13:30:14 +1300

    Can now enter digits

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -6,6 +6,29 @@ class App extends React.Component {
   state = {
     displayValue: '0'
   }
+
+  inputDigit = digit => {
+    const { displayValue } = this.state
+
+    this.setState({
+      displayValue: displayValue === '0' ? String(digit) : displayValue + digit
+    })
+  }
+
+  inputDecimal = decimal => {
+    const { displayValue } = this.state
+
+    //prevents more than one decimal being entered
+    if(displayValue.includes(decimal) === false) {
+      this.setState({
+        displayValue: displayValue + decimal
+      })
+    }
+    
+  }
+
+
+
   render() {
     const { displayValue } = this.state
     return (
@@ -19,20 +42,20 @@ class App extends React.Component {
           <button onClick={this.handleClick} className="btn btn__operator" value="-">-</button>
           
 
-          <button onClick={this.handleClick} className="btn" value="7">7</button>
-          <button onClick={this.handleClick} className="btn" value="8">8</button>
-          <button onClick={this.handleClick} className="btn" value="9">9</button>
-          <button onClick={this.handleClick} className="btn btn__operator" value="+">+</button>
+          <button onClick={ () => this.inputDigit(7)} className="btn" value="7">7</button>
+          <button onClick={ () => this.inputDigit(8)} className="btn" value="8">8</button>
+          <button onClick={ () => this.inputDigit(9)} className="btn" value="9">9</button>
+          <button  className="btn btn__operator" value="+">+</button>
           
-          <button onClick={this.handleClick} className="btn" value="4">4</button>
-          <button onClick={this.handleClick} className="btn" value="5">5</button>
-          <button onClick={this.handleClick} className="btn" value="6">6</button>
-          <button onClick={this.handleClick} className="btn btn__decimal" value=".">.</button>
+          <button onClick={ () => this.inputDigit(4)} className="btn" value="4">4</button>
+          <button onClick={ () => this.inputDigit(5)} className="btn" value="5">5</button>
+          <button onClick={ () => this.inputDigit(6)} className="btn" value="6">6</button>
+          <button onClick={ () => this.inputDecimal('.')} className="btn btn__decimal" value=".">.</button>
 
-          <button onClick={this.handleClick} className="btn" value="7">1</button>
-          <button onClick={this.handleClick} className="btn" value="7">2</button>
-          <button onClick={this.handleClick} className="btn" value="7">3</button>
-          <button onClick={this.handleClick} className="btn" value="7">0</button>
+          <button onClick={ () => this.inputDigit(1)} className="btn" value="7">1</button>
+          <button onClick={ () => this.inputDigit(2)} className="btn" value="7">2</button>
+          <button onClick={ () => this.inputDigit(3)} className="btn" value="7">3</button>
+          <button onClick={ () => this.inputDigit(0)} className="btn" value="7">0</button>
           
           <button onClick={this.handleClick} className="btn btn__operator equals" value="=">=</button>
           </div>
